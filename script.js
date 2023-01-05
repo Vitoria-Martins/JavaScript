b544fa9844f6a4842a050c2aa6cee7616bd41a79
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2023-01-05 19:30:25 -0300

    projeto impar e par finalizado

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var addNum = prompt("Digite um numero");
-var total = n/2;
 
-if(addNum/2== 0){
-    alert("Esse numero é Par");
-}
-if(addNum/2 == 1){
+if(addNum %2 === 0){
+    alert("Esse numero é par");
+} else {
     alert("Esse numero é impar");
-} alert(total);
+}
+
+
