5da61fa151d5e5879c0804df4127b6864a60c7d5
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2023-01-05 19:01:59 -0300

    vs 1

---
+++ script.js
@@ -0,0 +1,9 @@
+var addNum = prompt("Digite um numero");
+var total = n/2;
+
+if(addNum/2== 0){
+    alert("Esse numero é Par");
+}
+if(addNum/2 == 1){
+    alert("Esse numero é impar");
+} alert(total);
