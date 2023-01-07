1845a45bd367bccbdf7eaeecc63af9aa2b3193dd
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2023-01-06 21:00:24 -0300

    tabela feita

--- Tabela_tabuada/script.js
+++ Tabela_tabuada/script.js
@@ -1,7 +1,22 @@
+/*
+
+TABUADA SIMPLES COM VALOR DO USUARIO
+
 var valor = prompt("Digite um valor")
 
-// 1 define inicio 11 define o final
+ 1 define inicio 11 define o final
 for(var a = 1; a < 11;  a++) {
     document.write( valor + "x" + a + "="+   
     (valor * a) + "<br>");
 }
+
+*/
+
+for( var a = 1; a < 21; a++){
+    document.write("<p>Tabuada do " + a + "</p>" );
+    for(var b = 1; b < 21; b++){
+        document.write(a + "x" + b + "=" + (a*b) + "<br>");
+    }
+}
+
+
