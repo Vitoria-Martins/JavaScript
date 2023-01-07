7afa9d80b8e0e71587490150dc1d9f9c80f7124d
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2023-01-07 20:08:41 -0300

    desafio concluido

---
+++ km-milhas/script.js
@@ -0,0 +1,12 @@
+let kmDigitado = prompt("Adicione numero em Km");
+let cu = 10;
+
+function conversao(km){
+ let milhas = 1.609; 
+  return document.write(km/milhas , "<br>");
+ 
+}
+
+conversao(kmDigitado)
+
+conversao(cu)
