c2d27292fdb098dcffe8d9fcee09e5e6e272865b
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2023-01-11 22:52:20 -0300

    att

--- inversaoDeMatriz/script.js
+++ inversaoDeMatriz/script.js
@@ -3,6 +3,8 @@ function inversao(){
 
   let  arr = [ 'one', 'two', 'three', 'four', ' five']; 
 
+  console.log(arr);
+
  arr.reverse();
  return arr
  //return serve para aparecer o array que foi revertido
