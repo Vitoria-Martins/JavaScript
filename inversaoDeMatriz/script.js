6f38bd13e00ef87f830949afda8961741224407e
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2023-01-11 22:47:55 -0300

    desafio concluido

---
+++ inversaoDeMatriz/script.js
@@ -0,0 +1,14 @@
+
+function inversao(){
+
+  let  arr = [ 'one', 'two', 'three', 'four', ' five']; 
+
+ arr.reverse();
+ return arr
+ //return serve para aparecer o array que foi revertido
+}
+console.log(inversao());
+
+
+  
+
